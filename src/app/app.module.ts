0df4492235874d5ba3dbb020d91c78c3b833232a
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-05 16:17:53 -0300

    dashboard impl and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {AppRouters} from './app.routes';
+import {DataService} from './data/data.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import {AppRouters} from './app.routes';
     FlexLayoutModule,
     AppRouters,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
