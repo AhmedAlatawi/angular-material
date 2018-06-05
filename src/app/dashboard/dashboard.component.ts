582120e74bc26f91d3501e7e50058baab1d67c0a
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-05 16:46:38 -0300

    securing buttons

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import {DataService} from '../data/data.service';
 import {Post} from '../Post';
 import {DataSource} from '@angular/cdk/table';
 import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +11,20 @@ import {Observable} from 'rxjs/Observable';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, public auth: AuthService) {
   }
 
   displayedColumns = ['date_posted', 'title', 'category', 'delete'];
   dataSource = new PostDataSource(this.dataService);
+
+  deletePost(id) {
+    if (this.auth.isAuthenticated()) {
+      this.dataService.deletePost(id);
+      this.dataSource = new PostDataSource(this.dataService);
+    } else {
+      alert('Login in Before');
+    }
+  }
 }
 
 export class PostDataSource extends DataSource<any> {
